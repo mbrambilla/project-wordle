e27115c53b274462050efe10635e3f6b52190c5a
mbrambilla <mbrambilla@users.noreply.github.com>
2023-09-23 19:42:48 -0400

    Minor resturcture for put CSS into their own directory

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 
-import './reset.css';
-import './styles.css';
+import './css/reset.css';
+import './css/styles.css';
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
