223a47742560fcdcd5bcfc997748bd81e25914b3
mbrambilla <mbrambilla@users.noreply.github.com>
2023-09-24 20:46:47 -0400

    Stub out 4 letter support - because I can :)

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,11 @@
 export const NUM_OF_GUESSES_ALLOWED = 6;
+
+// Just for extra fun - let's play with word length
+// Possible todo: look into making this switchable by user
+// Going to need style tweaks too
+//
+// Valid word length is 4 or 5
+// Also must change import in `src/component/Game/Games.js` to be either
+// - import { WORDS_4 as WORDS } from '../../data';
+// - import { WORDS_5 as WORDS } from '../../data';
 export const NUM_WORD_LENGTH = 5;
